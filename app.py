3014f64886dd0420947ab099e87bb212a35b07d5
Venkat10292 <139848638+Venkat10292@users.noreply.github.com>
2025-06-17 00:07:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,9 @@
-from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/bot", methods=['POST'])
-def bot():
-    incoming_msg = request.form.get('Body')
-    print("User sent:", incoming_msg)
+@app.route('/')
+def home():
+    return "WhatsApp Bot Running"
 
-    # Create reply
-    resp = MessagingResponse()
-    msg = resp.message()
-    msg.body(f"మీ మెసేజ్ వచ్చింది: {incoming_msg} ✅")
-    
-    return str(resp)
-
-if __name__ == "__main__":
-    app.run(port=5000)
+if __name__ == '__main__':
+    app.run()
