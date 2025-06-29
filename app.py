7848618c2ad933965153f0e840253ce197dd7aa6
Venkat10292 <139848638+Venkat10292@users.noreply.github.com>
2025-06-29 15:33:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import numpy as np
 import uuid
 import pyotp
 from pg_db import init_db, is_user_authorized, add_user
-from smartapi import SmartConnect
+from smartapi.smartConnect import SmartConnect
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
